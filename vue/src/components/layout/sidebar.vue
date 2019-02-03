85bb728ee9d72faa1e52e82de7ccdda069b94df2
renwenzhi <1608955412@qq.com>
2019-02-03 13:09:30 +0800

    Update sidebar.vue
    用户列表渲染完成

--- vue/src/components/layout/sidebar.vue
+++ vue/src/components/layout/sidebar.vue
@@ -6,21 +6,12 @@
         <span>用户管理</span>
       </template>
       <el-menu-item-group>
-        <template slot="title">分组一</template>
-        <el-menu-item index="1-1">选项1</el-menu-item>
-        <el-menu-item index="1-2">选项2</el-menu-item>
+        <el-menu-item index="/users">用户列表</el-menu-item>
       </el-menu-item-group>
-      <el-menu-item-group title="分组2">
-        <el-menu-item index="1-3">选项3</el-menu-item>
-      </el-menu-item-group>
-      <el-submenu index="1-4">
-        <template slot="title">选项4</template>
-        <el-menu-item index="1-4-1">选项1</el-menu-item>
-      </el-submenu>
     </el-submenu>
     <el-menu-item index="2">
       <i class="el-icon-menu"></i>
-      <span slot="title">导航二</span>
+      <span slot="title">权限管理</span>
     </el-menu-item>
     <el-menu-item index="3" disabled>
       <i class="el-icon-document"></i>
@@ -35,8 +26,8 @@
 
 <script>
 export default {
-    
-};
+
+}
 </script>
 
 <style>
