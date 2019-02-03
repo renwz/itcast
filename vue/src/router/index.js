ec598b69bb637c18c9b120b667a3ef00af8a1743
renwenzhi <1608955412@qq.com>
2019-02-03 13:09:44 +0800

    Update index.js
    用户列表渲染完成

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -3,11 +3,16 @@ import Router from 'vue-router'
 
 import Login from '@/components/login/login.vue'
 import Home from '@/components/home/home.vue'
+import User from '@/components/right/users.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    {
+      path: '/',
+      redirect: '/login'
+    },
     {
       name: 'login',
       path: '/login',
@@ -16,7 +21,10 @@ export default new Router({
     {
       name: 'home',
       path: '/home',
-      component: Home
+      component: Home,
+      children: [
+        { path: '/users', component: User }
+      ]
     }
   ]
 })
