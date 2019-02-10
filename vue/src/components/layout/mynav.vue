8c39cb61f39f8603f2d25f5ba3d5c85ee69ac24f
renwenzhi <1608955412@qq.com>
2019-02-10 14:48:11 +0800

    Create mynav.vue

---
+++ vue/src/components/layout/mynav.vue
@@ -0,0 +1,18 @@
+<template>
+     <el-breadcrumb separator-class="el-icon-arrow-right" class="nav">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>{{firstNav}}</el-breadcrumb-item>
+      <el-breadcrumb-item>{{lastNav}}</el-breadcrumb-item>
+    </el-breadcrumb>
+</template>
+
+<script>
+export default {
+  name: 'myNav',
+  props: ['firstNav', 'lastNav']
+}
+</script>
+
+<style>
+
+</style>
